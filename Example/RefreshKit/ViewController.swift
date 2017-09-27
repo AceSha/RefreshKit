2f220559ce671d8221dd0c6adbc25a1a7e4f58db
“AceSha” <81556205@qq.com>
2017-09-27 10:14:53 +0800

    Initial commit

---
+++ Example/RefreshKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RefreshKit
+//
+//  Created by 81556205@qq.com on 09/27/2017.
+//  Copyright (c) 2017 81556205@qq.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
